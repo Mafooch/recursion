76213b31c3119a646741ecd3e527a0e55d502ca2
Mafooch <jpmaff@yahoo.com>
2015-05-10 13:54:47 -0400

    add recursive append and reverse append

---
+++ append.rb
@@ -0,0 +1,17 @@
+def append(ary, n)
+  return ary if n < 0
+  ary << n
+  append(ary, n - 1)
+end
+
+print append([2], 3) # => [2, 3, 2, 1, 0]
+
+def reverse_append(ary, n)
+  return ary if n < 0
+  # we just make the recursive call before appending
+  reverse_append(ary, n - 1)
+  ary << n
+  ary
+end
+
+print reverse_append([2], 3) # => [2, 0, 1, 2, 3]
